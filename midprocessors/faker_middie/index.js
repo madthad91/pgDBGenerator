78bcc328c888b1af5c4b9923c343f0c66761648a
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-11-26 23:34:24 -0600

    starting faker_middie. no uuid supported. all shorthands supported, foreign key placeholder placed. primary key support added

---
+++ midprocessors/faker_middie/index.js
@@ -0,0 +1,7 @@
+const faker = require('faker');
+const fs = require('fs');
+
+for(let i = 0; i<500; i++){
+
+const s1_t1_pk = faker.getNumber();
+fs.appendFileSync(`insert into s1.t1(c1, c2) values (${s1_t1_pk}, <%t1~_~c2~_~s2_t2_pk~_~null~_~s1_t1_pk%>);`);
---
+++ midprocessors/faker_middie/validator.js
@@ -0,0 +1,81 @@
+module.exports = { 
+  validateMyStuff: function(dataType) {
+    switch (dataType) {
+      case 'p':
+        return 'int8 PRIMARY KEY';
+      case 'i':
+        return 'int8';
+      case 'v':
+        return 'varchar(255)';
+      case 't':
+        return 'text';
+      case 'd':
+        return 'timestamp';
+      default:
+        let search = [
+          'bigint',
+          'bigserial',
+          'bit',
+          'bit',
+          'boolean',
+          'box',
+          'bytea',
+          'character',
+          'character',
+          'cidr',
+          'circle',
+          'date',
+          'double',
+          'inet',
+          'integer',
+          'interval',
+          'json',
+          'jsonb',
+          'line',
+          'lseg',
+          'macaddr',
+          'money',
+          'numeric',
+          'path',
+          'pg_lsn',
+          'point',
+          'polygon',
+          'real',
+          'smallint',
+          'smallserial',
+          'serial',
+          'text',
+          'time',
+          'time',
+          'timestamp',
+          'timestamp',
+          'tsquery',
+          'tsvector',
+          'txid_snapshot',
+          'uuid',
+          'xml',
+          'int8',
+          'serial8',
+          'varbit',
+          'bool',
+          'char',
+          'varchar',
+          'float8',
+          'int',
+          'int4',
+          'decimal',
+          'float4',
+          'int2',
+          'serial2',
+          'serial4',
+          'timetz',
+          'timestamptz',
+        ].find(data => {
+          return dataType.startsWith(data);
+        });
+
+        if (search) return dataType;
+        else return false;
+    }
+  }
+}
